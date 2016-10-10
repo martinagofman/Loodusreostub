b5a3ec2ba751deaee15f789b930e87080e218876
k3rtkukk <k3rtkukk@gmail.com>
2016-10-10 04:29:16 +0300

    #Kala :hover'i ja onClick funktsionaalsus lisatud

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-alert("loodus reostub");
+/*JS linti jaoks*/
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+//Kui dokument on laetud alles siis tegeleme järgenvaga
+$(function () {
+    "use strict";
+
+    /*tapaKala funktsioon vahetab kalale klikkides tema pildi ja lisab talle klassi, mis teeb ta nähtavaks*/
+    function tapaKala(kalaId, kalaKorjus) {
+        $(kalaId).click(function () {
+            $(kalaId.concat(' > img')).attr('src', 'pildid/kalad/'.concat(kalaKorjus));
+            $(this).addClass('surnud');
+        });
+    }
+    
+    tapaKala('#lohe', 'surnud_forell.png');
+    //tapaKala('kala ID', 'surnud_kala_pilt');
+
+});

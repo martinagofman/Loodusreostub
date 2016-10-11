fb83b847cf13942f263d3384ee4b73ef358cc5ce
k3rtkukk <k3rtkukk@gmail.com>
2016-10-12 02:59:37 +0300

    Parallax induwde funktsionaalsus lisatud

--- scripts.js
+++ scripts.js
@@ -13,7 +13,24 @@ $(function () {
             $(this).addClass('surnud'); /*lisab klassi*/
         });
     }
-    
+
+    /*parallaxScroll funktsioonis teostatakse kihtide liigutamine erinevatel kiirustel*/
+    function parallaxScroll(scrollitavElement) {
+        var scrolled = $(scrollitavElement).scrollTop();
+        $('#parallax-bg1').css('top', (Number(0) - (scrolled * 0.25)) + 'px');
+        $('#parallax-bg2').css('top', (Number(0) - (scrolled * 0.5)) + 'px');
+        $('#parallax-bg3').css('top', (Number(0) - (scrolled * 0.75)) + 'px');
+    }
+
+    /*Element seotakse 'scroll' eventiga, kus teostatakse parallaxScroll()*/
+    function siduja(element) {
+        $(element).bind('scroll', function (e) {
+            parallaxScroll(this);
+        });
+    }
+
+    siduja(".taevas");
+
     tapaKala('#forell', 'surnud_forell.png');
     tapaKala('#kammeljas', 'surnud_forell.png');
     /*tapaKala('#kammeljas', 'surnud_forell.png');*/
